860a83731868b334f8b80f4fbc59c84d4f146956
Jasiel <jasieloficial@hotmail.com>
2025-01-23 14:32:52 -0300

    feat: add middleware for subdomain handling and configure experimental middleware prefetch

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const host = request.headers.get("host") || ""; // Obtem o host atual
+  const mainDomain = process.env.NEXT_PUBLIC_FRONTEND || "revendaja.vercel.app"; // Domínio principal
+
+  if (host !== mainDomain) {
+    const subdomain = host.split(".")[0]; // Pega o subdomínio
+    request.nextUrl.searchParams.set("subdomain", subdomain); // Adiciona o subdomínio aos parâmetros
+  }
+
+  return NextResponse.next();
+}
