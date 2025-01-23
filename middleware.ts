0a7c0df723fd9c3c443fa8b573dbac33f9c10816
Jasiel <jasieloficial@hotmail.com>
2025-01-23 14:47:24 -0300

    feat: add comment to clarify continuation of request flow in middleware

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export function middleware(request: NextRequest) {
     request.nextUrl.searchParams.set("subdomain", subdomain); // Adiciona o subdomínio aos parâmetros
   }
 
-  return NextResponse.next();
+  return NextResponse.next(); // Continua o fluxo
 }
